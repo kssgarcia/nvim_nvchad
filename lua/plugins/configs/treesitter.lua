a1b785bf7b99ff725e1c0241b2b582d4002c63ca
kssgarcia <kssgarcia@outlook.com>
2024-04-01 09:44:21 -0500

    first commit

---
+++ lua/plugins/configs/treesitter.lua
@@ -0,0 +1,17 @@
+local options = {
+  ensure_installed = { "lua" },
+  highlight = {
+    enable = true
+  },
+  indent = {
+    enable = true
+  },
+  autopairs = {
+    enable = true
+  },
+  autotag = {
+    enable = true
+  },
+}
+
+return options
